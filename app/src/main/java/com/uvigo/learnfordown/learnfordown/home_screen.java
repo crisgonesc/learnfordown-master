49ae132d647cfee3c0df581026632e61c5458300
cris_ <cristinagonzalezescudeiro@gmail.com>
2017-02-04 13:12:57 +0100

    Testing commit from Android Studio

--- app/src/main/java/com/uvigo/learnfordown/learnfordown/home_screen.java
+++ app/src/main/java/com/uvigo/learnfordown/learnfordown/home_screen.java
@@ -1,7 +1,12 @@
 package com.uvigo.learnfordown.learnfordown;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.ImageButton;
+
 
 public class home_screen extends AppCompatActivity {
 
@@ -9,5 +14,27 @@ public class home_screen extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home_screen);
+    }
+    /** Called when the user clicks the Send button */
+    public void sendMessage(View view) {
+        // Do something in response to button
+        Intent intent = new Intent(this, MenuLectura1.class);
+        startActivity(intent);
+    }
+    public void sendMessage2(View view) {
+        // Do something in response to button
+        Intent intent = new Intent(this, DisplayMessageActivity.class);
+        startActivity(intent);
+    }
+
+    public void sendMessage3(View view) {
+        // Do something in response to button
+        Intent intent = new Intent(this, DisplayMessageActivity.class);
+        startActivity(intent);
+    }
+    public void sendMessage4(View view) {
+        // Do something in response to button
+        Intent intent = new Intent(this, DisplayMessageActivity.class);
+        startActivity(intent);
     }
 }
